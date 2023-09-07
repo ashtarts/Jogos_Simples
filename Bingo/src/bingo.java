c5e4b85e3113099783b0d9c6d832ff853432b78d
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2023-09-07 16:10:35 -0300

    Add files via upload

---
+++ Bingo/src/bingo.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class bingo {
+    private List<Integer> sortear;
+    private List<Integer> disp;
+    private List<Integer> cartela;
+
+    public bingo() {
+        sortear = new ArrayList<>();
+        disp = new ArrayList<>();
+        cartela = new ArrayList<>();
+
+        for (int i = 1; i <= 75; i++) {
+            disp.add(i);
+        }
+
+        Collections.shuffle(disp);
+
+        for (int i = 0; i < 25; i++) {
+            cartela.add(disp.remove(0));
+        }
+    }
+
+    public int sorteio() {
+        if (disp.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Todos os números já foram sorteados!");
+            return -1;
+        }
+
+        int numeroSorteado = disp.remove(0);
+        sortear.add(numeroSorteado);
+
+        marcarCartela(numeroSorteado);
+
+        return numeroSorteado;
+    }
+
+    public void print() {
+        JOptionPane.showMessageDialog(null, "Números sorteados: " + sortear);
+    }
+
+    public void mostrar() {
+        StringBuilder cartelaStr = new StringBuilder("Sua cartela:\n");
+
+        for (int i = 0; i < cartela.size(); i++) {
+            int numero = cartela.get(i);
+            String marcacao = numero == -1 ? "X" : Integer.toString(numero);
+
+            cartelaStr.append(String.format("%2s ", marcacao));
+
+            if ((i + 1) % 5 == 0) {
+                cartelaStr.append("\n");
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, cartelaStr.toString());
+    }
+
+    public void marcarCartela(int numeroSorteado) {
+        for (int i = 0; i < cartela.size(); i++) {
+            if (cartela.get(i) == numeroSorteado) {
+                cartela.set(i, -1);
+                break;
+            }
+        }
+    }
+    }
