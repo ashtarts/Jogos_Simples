7c5692c7bb262194a4ac835ffa87ec5dd15513b7
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2023-09-20 18:07:17 -0300

    Update Main.java

--- jogodaVelha/src/Main.java
+++ jogodaVelha/src/Main.java
@@ -1,7 +1,16 @@
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Jogo da Velha");
         Jogo jogo = new Jogo();
-        jogo.jogar();
+        frame.add(jogo);
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
